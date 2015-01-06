c7552a7bdd12df894695c2fb216972ace55c9c1a
unknown <DPerez@ABC-CED-1DNMPP1.abcconsulting.com.ar>
2015-01-06 12:10:40 -0300

    Prevent basic auth popup when running test against browser.  Replace url with valid username and password

--- TestCateringManagerConnection.js
+++ TestCateringManagerConnection.js
@@ -1,6 +1,6 @@
 ﻿describe('Test Catering Manager Connection', function () {
     it('Verify that the Catering Manager page connects', function () {
-        browser.get('http://cateringmanagerqa.chipotle.esc/Order/CreateOrder');
+        browser.get("http://username:password@cateringmanagerqa.chipotle.esc/Order/CreateOrder");
 
         browser.getTitle(function(title){
             assert(title === 'Create Order - Chipotle Catering');
