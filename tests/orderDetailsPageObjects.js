9052324e58857f821014ddcf87ae80a91bcc1316
Mike Levan <mlevan@chipotle.com>
2015-01-12 16:56:39 +0000

    orderDetailsPageObjects.js deleted online with Bitbucket

--- tests/orderDetailsPageObjects.js
+++
@@ -1,6 +0,0 @@
-var OrderDetailsPageObjects = function() {
-    this.pickupDateTxt = element(by.id("txtPickUpDate"));
-    this.pickupDateNextMonthIcon = element(by.css("span.ui-icon.ui-icon-circle-triangle-e"));
-    this.pickupTimeTxt = element(by.id("txtPickUpTime"));
-};
-module.exports = OrderDetailsPageObjects;
