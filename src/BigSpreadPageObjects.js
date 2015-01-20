093488805032ac6669c7ffdf5797ee4553afc262
MikeLevan <mlevan@chipotle.com>
2015-01-20 15:26:54 -0700

    Cleaning up unused code

--- src/BigSpreadPageObjects.js
+++ src/BigSpreadPageObjects.js
@@ -4,21 +4,11 @@ var helper = require('../src/Helper.js');
 
 var BigSpreadPage = function (){
     this.bigSpreadButton = element(by.css('[ng-click="addOrderItem(selectedRestaurant.Categories[1].MenuItems[1])"]'));
-    this.itemsOrderList = element.all(by.repeater('orderItem in order.OrderItems'));
-    this.bigSpreadQtyText = element(by.name("OrderItemQty"));
 };
 
 
 BigSpreadPage.prototype = Object.create({}, {
     clickBigSpreadButton : { value: function () { return this.bigSpreadButton.click(); }},
-    getBigSpreadQtyText : { value: function() { return this.bigSpreadQtyText.getText(); }},
-    getLastItemsOrderList : { get: function() { return this.itemsOrderList; }},
-    typeBigSpreadQtyText : { value: function(keys) {
-        element.all(by.repeater('orderItem in order.OrderItems')).last().then(function(row) {
-            var lastQtyElement = row.element(by.name("OrderItemQty"));
-            lastQtyElement.sendKeys(keys);
-        })
-    }},
     selectBigSpreadOptionFirst : { value: function(option) {
         element.all(by.repeater('orderItem in order.OrderItems')).last().then(function(row) {
             var lastOptionFirst = row.element(by.model('orderItem.OrderSubItems[0].SubMenuItemId'));
